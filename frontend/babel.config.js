c15a15d69adfade54e9da5b74bf427b0bb6f0d73
Vivek <vivekpradhan736@gmail.com>
2024-06-30 18:29:46 +0530

    new commit

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,11 @@
-plugins: [
-    ...
-    require('@babel/plugin-proposal-private-property-in-object').default,
-    require('@babel/plugin-proposal-private-methods').default
-];
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+    ],
+    plugins: [
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  };
